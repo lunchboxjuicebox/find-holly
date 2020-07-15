209efe9376eefd98ca1ad17964f5f5fe3c901e49
Francesca Castleton <csinocruz@gmail.com>
2020-07-14 22:40:43 -0700

    unlock screen functionality is working

--- main.js
+++ main.js
@@ -1,17 +1,6 @@
 $(document).ready(function(){
     createTextMessages();
-    //$('.back-button, .individual-back-button').click(playTinyClick);
+    $('#unlock-button').click(function(){
+        $('div.unlock-screen').css('display','none');
+    });
 });
-
-// var tinyClick = new Audio('assets/sounds/tiny-button-push.mp3');
-
-// function playTinyClick(){
-//     setTimeout(function(){
-//         tinyClick.play();
-
-//         setTimeout(function(){
-//             tinyClick.pause();
-//             tinyClick.currentTime = 0;
-//         }, 1000)
-//     });
-// }

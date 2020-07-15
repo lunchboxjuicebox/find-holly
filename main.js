65a03b10eea0817a397d6cbb77e52b407f016163
Francesca Castleton <csinocruz@gmail.com>
2020-07-15 16:57:04 -0700

    trying it without sound bc button is not working on live mobile

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
     createTextMessages();
     $('#unlock-button').click(function(){
-        playUnlockSound();
+        //playUnlockSound();
         $('div.unlock-screen').css('display','none');
     });
     $('#lock-button').click(function(){
-        playUnlockSound();
+        //playUnlockSound();
         $('div.unlock-screen').css('display','block');
     });
 });
 
-var unlockSound = new Audio('assets/sounds/shotgun-reload.mp3');
+// var unlockSound = new Audio('assets/sounds/shotgun-reload.mp3');
 
-function playUnlockSound(){
-    unlockSound.play();
-    setTimeout(function(){
-        unlockSound.pause();
-    },
-    455);
-}
+// function playUnlockSound(){
+//     unlockSound.play();
+//     setTimeout(function(){
+//         unlockSound.pause();
+//     },
+//     455);
+// }

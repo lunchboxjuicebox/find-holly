3949c3b7e76cb5a18c1019b730f28fad4a1d4a0d
Francesca Castleton <csinocruz@gmail.com>
2020-07-14 22:44:26 -0700

    unlock sound

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
     createTextMessages();
     $('#unlock-button').click(function(){
+        playUnlockSound();
         $('div.unlock-screen').css('display','none');
     });
 });
+
+var unlockSound = new Audio('assets/sounds/shotgun-reload.mp3');
+
+function playUnlockSound(){
+    unlockSound.play();
+}
